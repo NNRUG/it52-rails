1a02b017e4f24447859788d201f715ab3077a2f7
Mikhail Troshev <mishanga@yandex-team.ru>
2015-05-16 16:39:15 +0300

    views

--- app/assets/javascripts/event-map.js
+++ app/assets/javascripts/event-map.js
@@ -2,13 +2,15 @@ $(function() {
 
 $('.event-place').map(function() {
     var $link = $(this).find('a'),
-        //address = $link.html(),
-        address = 'Нижний Новгород, ул. Алексеевская, 10/16, БЦ Лобачевский Плаза, офис Яндекса',
+        address = $link.html(),
+        coords = ($link.attr('data-coords') || '').split(',').map(Number),
         $map = $(this).find('.event-map');
+
+    coords.length === 2 && coords[0] && coords[1] || (coords = null);
 
     $link.click(function(e) {
         $map.toggleClass('visible');
-        init($map);
+        $map.inited || init($map);
         return false;
     });
 
@@ -17,23 +19,35 @@ $('.event-place').map(function() {
         $map.attr('id', id);
 
         var myMap = new ymaps.Map(id, {
-                center: [56.326887, 44.005986],
-                zoom: 9
+                center: coords || [56.326887, 44.005986], // координаты центра НиНо
+                zoom: 9,
+                controls: ['smallMapDefaultSet']
             });
 
-        ymaps.geocode(address, {
-            kind: 'house',
-            boundedBy: myMap.getBounds(),
-            strictBounds: true,
-            results: 1
-        }).then(function(res) {
-            var firstGeoObject = res.geoObjects.get(0),
-                coords = firstGeoObject.geometry.getCoordinates(),
-                bounds = firstGeoObject.properties.get('boundedBy');
+        if (coords) {
+            myMap.geoObjects.add(
+                new ymaps.Placemark(
+                        coords,
+                        { balloonContent: address },
+                        { preset: 'islands#icon' }
+                    ));
+        } else {
+            ymaps.geocode(address, {
+                kind: 'house',
+                boundedBy: myMap.getBounds(),
+                strictBounds: true,
+                results: 1
+            }).then(function(res) {
+                var firstGeoObject = res.geoObjects.get(0),
+                    coords = firstGeoObject.geometry.getCoordinates(),
+                    bounds = firstGeoObject.properties.get('boundedBy');
 
-            myMap.geoObjects.add(firstGeoObject);
-            myMap.setBounds(bounds, { checkZoomRange: true });
-        });
+                myMap.geoObjects.add(firstGeoObject);
+                myMap.setBounds(bounds, { checkZoomRange: true });
+            });
+        }
+
+        $map.inited = true;
     }
 });
 
