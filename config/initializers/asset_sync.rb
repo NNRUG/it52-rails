f2483cedf4a89411f3d521bc582374219be15ae3
ujifyxbr <ujifyxbr@gmail.com>
2026-01-21 18:31:27 +0300

    yandex cloud

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,19 +1,37 @@
 # frozen_string_literal: true
 
 AssetSync.configure do |config|
+  enabled =
+    (Rails.env.production? || Rails.env.staging?) &&
+    ENV['aws_bucket'].to_s.strip != '' &&
+    ENV['aws_access_key_id'].to_s.strip != '' &&
+    ENV['aws_secret_access_key'].to_s.strip != ''
+
+  config.enabled = enabled if config.respond_to?(:enabled=)
+
+  # When not fully configured, do nothing (prevents build-time crashes during assets:precompile)
+  next unless enabled
+
   config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV.fetch('aws_access_key_id') { 'aws_access_key_id' }
-  config.aws_secret_access_key = ENV.fetch('aws_secret_access_key') { 'aws_secret_access_key' }
+  config.aws_access_key_id = ENV['aws_access_key_id']
+  config.aws_secret_access_key = ENV['aws_secret_access_key']
   # To use AWS reduced redundancy storage.
   # config.aws_reduced_redundancy = true
-  config.fog_directory = ENV.fetch('aws_bucket') { 'aws_bucket' }
+  config.fog_directory = ENV['aws_bucket']
 
   # Invalidate a file on a cdn after uploading files
   # config.cdn_distribution_id = "12345"
   # config.invalidate = ['file1.js']
 
   # Increase upload performance by configuring your region
-  config.fog_region = ENV.fetch('fog_region') { 'fog_region' }
+  config.fog_region = ENV.fetch('fog_region', 'ru-central1')
+  # Yandex Cloud Object Storage (S3-compatible) endpoint + path-style addressing
+  config.fog_path_style = true
+  config.fog_options = {
+    endpoint: ENV.fetch('fog_endpoint', 'https://storage.yandexcloud.net'),
+    host: ENV.fetch('fog_host', 'storage.yandexcloud.net'),
+    aws_signature_version: 4
+  }
   #
   # Don't delete files from the store
   # config.existing_remote_files = "keep"
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 # require 'carrierwave/storage/abstract'
-if remote_storage = (Rails.env.production? || Rails.env.staging?)
+remote_storage =
+  (Rails.env.production? || Rails.env.staging?) &&
+  ENV['aws_bucket'].to_s.strip != '' &&
+  ENV['aws_access_key_id'].to_s.strip != '' &&
+  ENV['aws_secret_access_key'].to_s.strip != ''
+
+if remote_storage
   require 'carrierwave/storage/fog'
 else
   require 'carrierwave/storage/file'
@@ -10,18 +16,24 @@ end
 CarrierWave.configure do |config|
   config.storage = remote_storage ? :fog : :file
 
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV.fetch('aws_access_key_id') { 'aws_access_key_id' },
-    aws_secret_access_key: ENV.fetch('aws_secret_access_key') { 'aws_secret_access_key' },
-    region: ENV.fetch('fog_region') { 'fog_region' },
-    host: ENV.fetch('fog_host') { 'fog_host' }
-  }
-  config.fog_use_ssl_for_aws = true
-  config.fog_directory    = ENV.fetch('aws_bucket') { 'aws_bucket' }
-  config.fog_public       = true
-  config.fog_attributes   = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
-  config.asset_host       = ENV.fetch('aws_host') { nil }
+  if remote_storage
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['aws_access_key_id'],
+      aws_secret_access_key: ENV['aws_secret_access_key'],
+      region: ENV.fetch('fog_region', 'ru-central1'),
+      # Yandex Cloud Object Storage (S3-compatible)
+      endpoint: ENV.fetch('fog_endpoint', 'https://storage.yandexcloud.net'),
+      host: ENV.fetch('fog_host', 'storage.yandexcloud.net'),
+      path_style: true,
+      aws_signature_version: 4
+    }
+    config.fog_use_ssl_for_aws = true
+    config.fog_directory    = ENV['aws_bucket']
+    config.fog_public       = true
+    config.fog_attributes   = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
+    config.asset_host       = ENV.fetch('aws_host', nil)
+  end
   config.cache_dir        = "#{Rails.root}/tmp/uploads"
 
   config.ignore_integrity_errors = false
