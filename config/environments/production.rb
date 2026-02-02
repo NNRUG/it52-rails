2955f6beacd9bee44be9c9f94e616d87fe9d22eb
ujifyxbr <ujifyxbr@gmail.com>
2026-02-02 13:38:01 +0300

    fix for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,9 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-  config.action_controller.asset_host = ENV.fetch('aws_host') { 'aws_host' }
+  # Set aws_host only when using a CDN/S3 (e.g. https://storage.yandexcloud.net/your-bucket).
+  # When unset, assets are served from the same domain (no broken "aws_host" placeholder).
+  config.action_controller.asset_host = ENV['aws_host'].presence
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
